3d1fa687a269d850e95faa8cfd0a572fce1f5891
aaravaggarwal <24aaggarwal@athenian.org>
2023-05-16 11:09:40 -0700

    got rid of goofy variables

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         //Frame n = new Frame(6,generateWord());
-        Grid n = new Grid(6,"APPLE");
+        Grid n = new Grid(6,generateWord());
     }
 
 
