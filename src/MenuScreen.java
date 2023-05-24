f1ca83c2bb97495133efd99a17e20cfea61f4614
aaravaggarwal <24aaggarwal@athenian.org>
2023-05-24 10:11:50 -0700

    fixed menu screen, made dropdowns

--- src/MenuScreen.java
+++ src/MenuScreen.java
@@ -5,32 +5,20 @@ import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 
 public class MenuScreen {
+    private boolean randColor;
 
     public MenuScreen (){
         JFrame frame = new JFrame("Wordle Menu");
         JPanel panel = new JPanel();
 
        // frame.setLayout(new BoxLayout(frame, BoxLayout.Y_AXIS));
-        JLabel field1 = new JLabel("Number of Grids");
-        panel.add(field1);
-        JButton nog1 = new JButton("1");
-        panel.add(nog1);
-        JButton nog2 = new JButton("2");
-        panel.add(nog2);
-        JButton nog3 = new JButton("3");
-        panel.add(nog3);
-        JButton nog4 = new JButton("4");
-        panel.add(nog4);
-        JLabel field2 = new JLabel("Number of Rows");
-        panel.add(field2);
-        JButton nor5 = new JButton("5");
-        panel.add(nor5);
-        JButton nor6 = new JButton("6");
-        panel.add(nor6);
-        JButton nor7 = new JButton("7");
-        panel.add(nor7);
-        JButton nor8 = new JButton("8");
-        panel.add(nor8);
+        JLabel label1 = new JLabel("Number of Grids");
+        panel.add(label1);
+
+        Integer[] gridAmtChoices = {1,2,4,8,16};
+        JComboBox<Integer> gridAmtDropdown = new JComboBox<Integer>(gridAmtChoices);
+        panel.add(gridAmtDropdown);
+
         JLabel field3 = new JLabel("Random Color Mode");
         panel.add(field3);
         JButton rcmon = new JButton("On");
@@ -39,19 +27,33 @@ public class MenuScreen {
         panel.add(rcmoff);
         JLabel field4 = new JLabel("Number of Letters");
         panel.add(field4);
+
+        Integer[] letterAmtChoices = {3,4,5,6,7,8};
+        JComboBox<Integer> letterAmtDropdown = new JComboBox<Integer>(letterAmtChoices);
+        panel.add(letterAmtDropdown);
+
         JButton enter = new JButton("ENTER");
         panel.add(enter);
         frame.add(panel);
         frame.pack();
+        randColor = false;
+        rcmon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                randColor = true;
+            }
+        });
+        rcmoff.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                randColor = false;
+            }
+        });
         enter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    boolean b = false;
-                    if (field3.getText().equals("t")){
-                        b = true;
-                    }
-                    MultiGrid grid = new MultiGrid(Integer.parseInt(field1.getText()), Integer.parseInt(field2.getText()), b, Integer.parseInt(field4.getText()));
+                    MultiGrid grid = new MultiGrid(gridAmtChoices[gridAmtDropdown.getSelectedIndex()], randColor, letterAmtChoices[letterAmtDropdown.getSelectedIndex()]);
                     frame.dispose();
                 } catch (FileNotFoundException ex) {
                     throw new RuntimeException(ex);
--- src/MultiGrid.java
+++ src/MultiGrid.java
@@ -7,12 +7,14 @@ public class MultiGrid{
     private JFrame frame;
     private JPanel bottomtextpanel;
     private int row;
-    public MultiGrid(int numGrids, int row, Boolean RandomColor, int letterAmt) throws FileNotFoundException {
+    public MultiGrid(int numGrids, Boolean RandomColor, int letterAmt) throws FileNotFoundException {
         this.numGrids = numGrids;
-        this.row=row;
+        row = letterAmt+1;
+        for (int i = 0; i<numGrids; i++){
+            row+=i;
+        }
         Grid[] gameGrids = new Grid[numGrids];
         JFrame frame = new JFrame("MultiWordle");
-        //JScrollPane scrl = new JScrollPane();
         JPanel panel = new JPanel();
         frame.setLayout(new BorderLayout());
         for (int i = 0; i < numGrids; i++){
@@ -30,18 +32,14 @@ public class MultiGrid{
                 gameGrids[i] = new Grid(row, Main.generateWordSeven());
             } else if (letterAmt == 8){
                 gameGrids[i] = new Grid(row, Main.generateWordEight());
-            } else {
-                gameGrids[i] = new Grid(row, "Apple");
             }
             panel.add(gameGrids[i].getPanelNoFrame());
         }
-        //scrl.add(panel);
-        //frame.add(scrl);
         frame.add(panel);
         if (RandomColor == false) {
             AnswerChecker a = new AnswerChecker(row, gameGrids);
             frame.add(a.getBottomtextpanel(), BorderLayout.SOUTH);
-            frame.setSize(500, 500);
+            frame.pack();
             frame.setVisible(true);
         } else {
             WrongColor a = new WrongColor(row, gameGrids);
